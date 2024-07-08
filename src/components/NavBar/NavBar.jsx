bf31e00a0592ecdbb31a3f63ddfecbd0e8eaeee5
Tomas Caggiano <tomascaggiano03@gmail.com>
2024-07-08 14:55:15 -0300

    arreglos 3

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -4,12 +4,13 @@ import Navbar from 'react-bootstrap/Navbar';
 import './NavBar.css'
 import CartWidget from '../CartWidget/CartWidget';
 import { Link, NavLink } from 'react-router-dom';
+import '../images/logo.png'
 
 function NavBar() {
   return (
     <header className="NavBar">
   <Link to="/e-commerce-ap-/">
-  <img width={100} src="/logo.png" alt="logo" />
+  <img width = {25} src='https://thumbs.dreamstime.com/z/dise%C3%B1o-vectorial-de-logotipo-negro-nike-sports-logo-vector-listo-para-imprimir-ilustraciones-183282273.jpg?ct=jpeg' alt="logo" />
   </Link>
     <NavLink to="/e-commerce-ap-/category/men's clothing">ropa</NavLink>
     <NavLink to="/e-commerce-ap-/category/electronics">electronica</NavLink>
