39516ffedaa7ade885f639cc7804ca17d8a1fb8c
Tomas Caggiano <tomascaggiano03@gmail.com>
2024-07-09 11:26:07 -0300

    pruebas 3

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
-  plugins: [react()],
   base: '/e-commerce-ap-/',
+  plugins: [react()],
 });
