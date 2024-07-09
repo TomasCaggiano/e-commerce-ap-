da373960489aacc1bb48d46330a5e2123e223ec8
Tomas Caggiano <tomascaggiano03@gmail.com>
2024-07-09 11:19:21 -0300

    pruebas 3

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,25 @@
-import NavBar from './components/NavBar/NavBar'
-import './App.css'
-import ItemListContainer from './components/ItemListContainer/ItemListContainer'
-import {BrowserRouter, Routes, Route} from "react-router-dom"
-import ItemDetailContainer from './components/ItemDetail/ItemDetailContainer'
-import CartProvider, { CartContext } from './components/CartContext/CartContext'
-import Cart from './components/Cart/Cart'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import NavBar from './components/NavBar/NavBar';
+import './App.css';
+import ItemListContainer from './components/ItemListContainer/ItemListContainer';
+import ItemDetailContainer from './components/ItemDetail/ItemDetailContainer';
+import CartProvider from './components/CartContext/CartContext';
+import Cart from './components/Cart/Cart';
 
 function App() {
   return (
     <CartProvider>
-    <BrowserRouter>
-    <NavBar/>
-    <Routes>
-      <Route path="/" element={<ItemListContainer/>}/>
-      <Route path="/category/:categoryId" element={<ItemListContainer/>}/>
-      <Route path='/product/:productId' element={<ItemDetailContainer/>}/>
-      <Route path='/cart' element={<Cart/>}></Route>
-      </Routes>
-    </BrowserRouter>
+      <BrowserRouter basename="/e-commerce-ap-/">
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<ItemListContainer />} />
+          <Route path="/category/:categoryId" element={<ItemListContainer />} />
+          <Route path='/product/:productId' element={<ItemDetailContainer />} />
+          <Route path='/cart' element={<Cart />} />
+        </Routes>
+      </BrowserRouter>
     </CartProvider>
-  )
+  );
 }
 
-export default App
+export default App;
